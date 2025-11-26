52ba9e1095068991a2f4f46516d4d78863df255f
Ohood <msohood@gmail.com>
2025-11-26 18:01:22 +0200

    MyWork

--- src/test/java/InfoTest.java
+++ src/test/java/InfoTest.java
@@ -1,87 +1,59 @@
 import Pages.InfoPage;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class InfoTest extends BaseTest {
 
     InfoPage myInfo;
-
     @BeforeMethod
     public void setUpPage() {
         myInfo = new InfoPage(driver);
     }
 
-    @Test(dataProvider ="personalInfo",dataProviderClass = TestData.class, priority = 1)
-    public void updatePersonalInfoTest( String middleName , String lastName, String otherId , String license , String dob ,String gender, String maritalStatus ){
-
+    @Test(dataProvider ="personalInfo", dataProviderClass = TestData.class, priority = 1)
+    public void updatePersonalInfoTest(String middleName, String lastName, String otherId,
+                                       String license, String dob, String gender, String maritalStatus) {
 
         myInfo.openMyInfoIfVisible();
 
-
-
-
-
-        // ---------- Update Fields ----------
         myInfo
                 .setMiddleName(middleName)
                 .setLastName(lastName)
                 .setOtherID(otherId)
                 .setLicense(license)
                 .setDateOfBirth(dob)
+                .setGender(gender)
+                .setMaritalStatus(maritalStatus);
 
+        myInfo.saveInfo();
 
-        ;
-
-        // Save changes
-        myInfo.clickSave();
-
-        // ---------- Assertions ----------
-
-        //Assert.assertEquals(myInfo.getFirstName(), firstName, "First name not updated");
-        Assert.assertEquals(myInfo.getMiddleName(), middleName, "Middle name not updated");
-        Assert.assertEquals(myInfo.getLastName(), lastName, "Last name not updated");
-        Assert.assertEquals(myInfo.getOtherID(), otherId, "Other ID not updated");
-        Assert.assertEquals(myInfo.getLicense(), license, "License not updated");
-        Assert.assertEquals(myInfo.getDateOfBirth(), dob, "Date of Birth not updated");
-        Assert.assertEquals(myInfo.getGender(), gender, "Gender not updated");
-        Assert.assertEquals(myInfo.getMaritalStatus(), maritalStatus, "Marital Status not updated");
-
+        InfoAssertions.assertPersonalInfo(myInfo, middleName, lastName,
+                otherId, license, dob, gender, maritalStatus);
     }
+
     @Test(priority = 2)
-    public void testUploadAttachment() throws Exception {
+    public void testUploadAttachment() {
+        String filePath = "C:\\Users\\Ohood\\IdeaProjects\\WebTestAutomationFramework3\\failedScreenshots\\ph.png";
 
-        String filePath = System.getProperty("user.dir") + "/testFiles/test-document.pdf";
-        String description = "Test Upload File";
-        String fileName = "ph.png";
+        myInfo.uploadAttachment(filePath, "Test Upload File");
 
-        myInfo.uploadAttachment("C:\\Users\\Ohood\\IdeaProjects\\WebTestAutomationFramework3\\failedScreenshots\\ph.png", description);
-
-        // Assertion: verify file is uploaded
-        Assert.assertTrue(myInfo.isAttachmentPresent(fileName),
-                "Attachment was not uploaded successfully!");
+      InfoAssertions.assertAttachmentExists(myInfo, "ph.png");
     }
 
     @Test(priority = 3)
-    public void testDeleteAttachment() throws Exception {
+    public void testDeleteAttachment() {
 
-        String fileName = "ph.png";
-        myInfo.deleteAttachment(driver, "ph.png");
-        // Assertion: verify file is deleted
-        Assert.assertTrue(myInfo.isAttachmentPresent(fileName),
-                "Attachment was not deleted successfully!");
+        myInfo.deleteAttachment("ph.png");
+
+        InfoAssertions.assertAttachmentNotExists(myInfo, "test.png");
     }
-  @Test(priority = 1)
-   public void testChangeProfilePicture() {
 
+    @Test(priority = 4)
+    public void testChangeProfilePicture() {
         myInfo.openMyInfo();
-
-      myInfo.openProfilePic();//
-      myInfo.uploadPicture("C:\\Users\\Ohood\\IdeaProjects\\WebTestAutomationFramework3\\failedScreenshots\\ph.png");
-
+        myInfo.openProfilePic();
+        myInfo.uploadPicture("C:\\Users\\Ohood\\IdeaProjects\\WebTestAutomationFramework3\\failedScreenshots\\ph.png");
         myInfo.savePicture();
-
         System.out.println("✔ Profile picture updated successfully!");
     }
-
 }
