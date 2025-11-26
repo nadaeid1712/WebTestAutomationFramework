2882b69059928467bd47a46523d1d846009a5f6d
shaimaadahab59 <shaimaadahab59@gmail.com>
2025-11-26 16:54:43 +0200

    All file

--- src/main/java/Pages/PimEmployeePage.java
+++ src/main/java/Pages/PimEmployeePage.java
@@ -10,15 +10,16 @@ public class PimEmployeePage {
 
     //driver
     WebDriver pimEmployeeDriver;
-    List<WebElement> employeeSearch=pimEmployeeDriver.findElements(By.cssSelector("input[placeholder=\"Type for hints...\"]"));
-    int search =employeeSearch.size();
-
-    List<WebElement> selectEmployee =pimEmployeeDriver.findElements(By.cssSelector("i[class=\"oxd-icon bi-check oxd-checkbox-input-icon\"]"));
-    int selectCount=selectEmployee.size();
+//    List<WebElement> employeeSearch=pimEmployeeDriver.findElements(By.cssSelector("input[placeholder=\"Type for hints...\"]"));
+//    int search =employeeSearch.size();
+//
+//    List<WebElement> selectEmployee =pimEmployeeDriver.findElements(By.cssSelector("i[class=\"oxd-icon bi-check oxd-checkbox-input-icon\"]"));
+//    int selectCount=selectEmployee.size();
 
 
 
     //Locator
+    By pimLocator= By.cssSelector("a[href='/web/index.php/pim/viewPimModule']");
     By searchEmployeeButtonLocator=By.xpath("//a[text()=\"Employee List\"]");
     By searchButtonLocator=By.cssSelector("button[class=\"oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space\"]");
     By resetButtonLocator=By.cssSelector("button[type=\"reset\"]");
@@ -34,12 +35,14 @@ public class PimEmployeePage {
     public PimEmployeePage(WebDriver driver){pimEmployeeDriver=driver;}
     //Methods
     public void searchEmployee(){
+        pimEmployeeDriver.findElement(pimLocator).click();
         pimEmployeeDriver.findElements(searchEmployeeButtonLocator).get(0).sendKeys("A");
         pimEmployeeDriver.findElement(searchButtonLocator).click();
 
     }
     public void resetSearch(){
-        pimEmployeeDriver.findElements(searchEmployeeButtonLocator).get(0).sendKeys("A");
+
+        searchEmployee();
         pimEmployeeDriver.findElement(resetButtonLocator).click();
 
     }
@@ -51,7 +54,10 @@ public class PimEmployeePage {
         pimEmployeeDriver.findElement(yesDeleteButton).click();
     }
     public void cancelDeleteEmpolyees(){
-        deleteEmployees();
+        pimEmployeeDriver.findElements(selectAllButtonLocator).get(1).click();
+        pimEmployeeDriver.findElements(selectAllButtonLocator).get(2).click();
+        pimEmployeeDriver.findElements(selectAllButtonLocator).get(3).click();
+        pimEmployeeDriver.findElement(deleteSelectedButtonLocator).click();
         pimEmployeeDriver.findElement(noCancelButton).click();
 
     }
