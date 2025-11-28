12b8cf51327e31753aa16011bdef438c6cc07e6d
shaimaadahab59-design <shaimaadahab59@gmail.com>
2025-11-28 05:38:30 +0200

    All files

--- Automation/test/java/AdminTest.java
+++ Automation/test/java/AdminTest.java
@@ -1,12 +1,6 @@
 
-import Pages.AdminPage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
-import org.testng.annotations.*;
 
 
 public class AdminTest extends BaseTest {
