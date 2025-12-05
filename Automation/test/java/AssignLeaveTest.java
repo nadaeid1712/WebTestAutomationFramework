19b97dfa2983d290d339cea7c1560a964926e336
Nada Ahmed <nada.ahmmed1996@gmail.com>
2025-12-05 13:06:20 +0200

    all files

--- Automation/test/java/AssignLeaveTest.java
+++ Automation/test/java/AssignLeaveTest.java
@@ -4,20 +4,23 @@ import org.testng.annotations.Test;
 
 public class AssignLeaveTest extends BaseTest {
 
-    @Test(priority = 1)
+    @Test(priority = 2)
 
     public void navigateToAssignLeavePage() {
 Allure.step("Assign Leave Page");
 new AssignLeavePage(driver).checkAssignLeavePage();
 // Assertion //
-
+verifySuccessMessage();
+Allure.step("Navigate to Assign Leave Page");
     }
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void createLeave() {
         Allure.step("Create Leave");
         new AssignLeavePage(driver).createLeave();
 
         // Assertion //
+        verifySuccessMessage();
+        Allure.step("Navigate to Create Leave Page");
 
 
 
